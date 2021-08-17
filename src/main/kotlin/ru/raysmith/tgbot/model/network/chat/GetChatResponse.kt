d4059d9493a00de778fd557d145730c49d4dc921
RaySmith-ttc <raysmith.ttcreate@gmail.com>
2021-08-17 23:49:48 +0500

    fix getChat response

--- src/main/kotlin/ru/raysmith/tgbot/model/network/chat/GetChatResponse.kt
+++ src/main/kotlin/ru/raysmith/tgbot/model/network/chat/GetChatResponse.kt
@@ -7,16 +7,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class GetChatResponse(
     @SerialName("ok") val ok: Boolean,
-    @SerialName("result") val result: Result
+    @SerialName("result") val result: Chat
 )
-
-@Serializable
-data class Result(
-    @SerialName("all_members_are_administrators") val allMembersAreAdministrators: Boolean? = null,
-    @SerialName("id") val id: Int,
-    @SerialName("invite_link") val inviteLink: String? = null,
-    @SerialName("permissions") val permissions: Permissions? = null,
-    @SerialName("title") val title: String? = null,
-    @SerialName("type") val type: ChatType
-)
-
