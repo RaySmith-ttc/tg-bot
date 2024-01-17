94e2664e1bdf8a2690237d846b132cad08d05904
Ray Smith <raysmith.ttcreaet@gmail.com>
2024-01-18 06:01:08 +0800

    chore: remove retrofit code and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     alias(libs.plugins.kotlinx.serialization)
-    kotlin("jvm") version "2.0.0-Beta1"
+    kotlin("jvm") version "2.0.0-Beta2"
     id("maven-publish")
 }
 
@@ -57,11 +57,6 @@ dependencies {
     implementation("io.ktor:ktor-client-okhttp-jvm:2.3.4")
 
     // Network
-    val kotlinxSerializationVersion = "1.5.1"
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.bundles.ktor.client.jvm)
     implementation(libs.ktor.client.okhttp)
@@ -80,7 +75,6 @@ dependencies {
     testImplementation("org.mockito:mockito-core:4.8.0")
     testImplementation(libs.konsist)
     testImplementation(libs.ktor.server.core.jvm)
-    testImplementation(libs.ktor.server.netty)
     testImplementation(libs.ktor.server.netty)
     testImplementation(libs.ktor.network.tlsCertificates)
     testImplementation(libs.log4j.slf4j18.impl)
--- src/main/kotlin/ru/raysmith/tgbot/network/NetworkUtils.kt
+++ src/main/kotlin/ru/raysmith/tgbot/network/NetworkUtils.kt
@@ -1,31 +1,8 @@
 package ru.raysmith.tgbot.network
 
-import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
-import kotlinx.serialization.serializer
-import retrofit2.Converter
-import retrofit2.Retrofit
-import java.lang.reflect.Type
 
 internal object NetworkUtils {
-
-    class ConverterFactory : Converter.Factory() {
-        override fun stringConverter(type: Type, annotations: Array<Annotation>, retrofit: Retrofit): Converter<*, String>? {
-            if (type !is Class<*>) return null
-            if (type.isEnum) return EnumConverter
-
-            return when (type) {
-                String::class.java -> null
-                else -> jsonConverter { serializer(type) }
-            }
-        }
-    }
-
-    val EnumConverter = Converter<Enum<*>, String> { it.name.lowercase() }
-
-    fun <T> jsonConverter(block: () -> KSerializer<T>) =
-        Converter<T, String> { TelegramApi.json.encodeToString(block(), it) }
-
     private inline fun <reified A : Annotation> Enum<*>.getEnumFieldAnnotation(): A? =
         javaClass.getDeclaredField(name).getAnnotation(A::class.java)
 
