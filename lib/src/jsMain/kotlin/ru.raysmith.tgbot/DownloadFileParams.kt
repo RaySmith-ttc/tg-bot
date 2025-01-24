d095427ea98e149aa6a92f7f458d86983361ce1f
Ray Smith <raysmith.ttcreate@gmail.com>
2025-01-24 20:47:38 +0600

    feat: update webapp to Telegram API v. 8.2

--- lib/src/jsMain/kotlin/ru.raysmith.tgbot/DownloadFileParams.kt
+++ lib/src/jsMain/kotlin/ru.raysmith.tgbot/DownloadFileParams.kt
@@ -1,6 +1,13 @@
 package ru.raysmith.tgbot
 
-/** This object describes the parameters for the file download request. */
+/**
+ * This object describes the parameters for the file download request.
+ *
+ * > **Note:** To ensure consistent file download behavior across platforms, it is recommended to include the HTTP header
+ * `Content-Disposition: attachment; filename="<file_name>"` in the server response. This header helps prompt the
+ * download action and suggests a file name for the downloaded file, especially on web platforms where forced downloads
+ * cannot always be guaranteed.
+ * */
 external interface DownloadFileParams {
 
     /** The HTTPS URL of the file to be downloaded. */
